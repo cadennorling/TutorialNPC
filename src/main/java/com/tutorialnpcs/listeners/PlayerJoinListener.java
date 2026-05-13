8bcdc1c96733ef22bf2e75578630ef2b1ad9b810
cadennorling <cadennorling@gmail.com>
2026-05-13 16:41:56 -0500

    Create PlayerJoinListener.java

---
+++ src/main/java/com/tutorialnpcs/listeners/PlayerJoinListener.java
@@ -0,0 +1,24 @@
+package com.tutorialnpcs.listeners;
+
+import com.tutorialnpcs.TutorialNPCsPlugin;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerJoinListener implements Listener {
+
+    private final TutorialNPCsPlugin plugin;
+
+    public PlayerJoinListener(TutorialNPCsPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
+            plugin.getPlayerProgressManager().loadPlayer(event.getPlayer());
+            plugin.getServer().getScheduler().runTask(plugin, () ->
+                    plugin.getHologramManager().updateForPlayer(event.getPlayer()));
+        });
+    }
+}
